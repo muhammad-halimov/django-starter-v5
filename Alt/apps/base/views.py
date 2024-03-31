ba8b29ae7d3ca3099ec12870efc69f4b3728b8c5
Muhammad Halimov <muhammadi.halimov@icloud.com>
2024-03-31 23:10:47 +0400

    Minor translate fixes

--- Alt/apps/base/views.py
+++ Alt/apps/base/views.py
@@ -28,7 +28,7 @@ def login_page(request):
             login(request, user)
             return redirect('main')
         except BaseException:
-            messages.error(request, "Почта либо пароль неправильны")
+            messages.error(request, "Email or Adress is incorrect")
 
     return render(request, 'base/login.htm')
 
@@ -48,7 +48,7 @@ def registration_page(request):
             login(request, user)
             return redirect('main')
         else:
-            messages.error(request, 'Ваш пароль является слабым, либо поля неправильны')
+            messages.error(request, "Your password is weak or Fields are filled incorrectly")
 
     context = {'form': form}
     return render(request, 'base/registration.htm', context)
